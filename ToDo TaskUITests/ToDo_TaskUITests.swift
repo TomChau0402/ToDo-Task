7e9c7e2a938ae2e541ab79b7264b29fc258c23da
Tom Chau <tung.t.t.chau@gmail.com>
2026-01-21 13:22:48 -0800

    MD 117 assignment 1

--- ToDo TaskUITests/ToDo_TaskUITests.swift
+++ ToDo TaskUITests/ToDo_TaskUITests.swift
@@ -17,6 +17,7 @@ final class ToDo_TaskUITests: XCTestCase {
         
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
+    
     func testLaunchInEnglish() {
         app.launchArguments = ["-AppleLanguage", "(en)"]
         app.launch()
@@ -25,6 +26,7 @@ final class ToDo_TaskUITests: XCTestCase {
         XCTAssertTrue(header.exists, "The english header of 'Who is working today' is not found")
         
     }
+    
     func testLaunchInSpanish() {
         app.launchArguments = ["-AppleLanguage", "(es)"]
         app.launch()
@@ -32,31 +34,31 @@ final class ToDo_TaskUITests: XCTestCase {
         let header = app.staticTexts["Selecciona el perfil de trabajo"]
         XCTAssertTrue(header.exists, "The Spanish header of 'Who is working today' is not found")
     }
-}
-//MARK 117 - 1
+    
+    //MARK 117 - 1
 
-//func testUserFlow() throws {
-//    let professorCard = app.buttons["proflieCard_Professor"]
-//    XCTAssertTrue(professorCard.waitForExistence(timeout: 5), "The professor card is not found")
-//    professorCard.tap()
-//    
-//    let addGroupButton = app.buttons["addGroupButton"]
-//    XCTAssertTrue(addGroupButton.isEmpty, "The add group button is not found")
-//    addGroupButton.tap()
-//    
-//    let groupNameField = app.textFields["newGroupNameField"]
-//    XCTAssertTrue(addGroupButton.waitForExistence(timeout: 5))
-//    
-//}
-//    let newGroupRow = app.buttons["groupRow_Testing Project"]
-//    XCTAssertTrue(newGroupRow.waitForExistence(timeout: 5), "The Test Project group should be visible")
-//    newGroupRow.tap()
-//
-//    let addTaskButton = app.buttons["addTaskButton"]
-//    XCTAssertTrue(addTaskButton.isEmpty, "The add task button is not found")
-//    addTaskButton.tap()
-//
-//    let taskTextField = app.textFields.firstMatch
-//    taskTextField.tap()
-//    taskTextField.typeText("Finish UI Test")
-//
+    func testUserFlow() throws {
+        let professorCard = app.buttons["proflieCard_Professor"]
+        XCTAssertTrue(professorCard.waitForExistence(timeout: 5), "The professor card is not found")
+        professorCard.tap()
+        
+        let addGroupButton = app.buttons["addGroupButton"]
+        XCTAssertTrue(addGroupButton.exists, "The add group button is not found")
+        addGroupButton.tap()
+        
+        let groupNameField = app.textFields["newGroupNameField"]
+        XCTAssertTrue(addGroupButton.waitForExistence(timeout: 5))
+        
+        let newGroupRow = app.buttons["groupRow_Testing Project"]
+        XCTAssertTrue(newGroupRow.waitForExistence(timeout: 5), "The Test Project group should be visible")
+        newGroupRow.tap()
+
+        let addTaskButton = app.buttons["addTaskButton"]
+        XCTAssertTrue(addTaskButton.exists, "The add task button is not found")
+        addTaskButton.tap()
+
+        let taskTextField = app.textFields.firstMatch
+        taskTextField.tap()
+        taskTextField.typeText("Finish UI Test")
+    }
+}
